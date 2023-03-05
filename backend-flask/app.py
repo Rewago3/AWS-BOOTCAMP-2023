8404fd133ad77d640bee418f7c84ba7797078bbf
Rewago3 <rewago3@gmail.com>
2023-03-05 03:07:40 +0000

    Added sampling rules(xray.json)

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,12 +22,22 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExporter
+#---xray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 
 #Honeycomb
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+#--X ray
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+
 # Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
@@ -36,6 +46,7 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+XRayMiddleware(app, xray_recorder)
 # Initialize automatic instrumentation with Flask
 #app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
