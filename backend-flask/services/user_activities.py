49022251eb2def8f7264b850d330b99d8c0f778c
Rewago3 <rewago3@gmail.com>
2023-03-05 05:31:42 +0000

    Implemented Cloud Watch Logs

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    segment = xray_recorder.begin_segment('user_activities')
+    #segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
@@ -10,11 +10,11 @@ class UserActivities:
 
     now = datetime.now(timezone.utc).astimezone()
     #xray
-    dict = {
-        "now":now.isoformat()
-    }
-    subsegment = xray_recorder.begin_subsegment('mock-data')  
-    subsegment.put_metadata('key', dict, 'namespace')
+    # dict = {
+    #     "now":now.isoformat()
+    # }
+    # subsegment = xray_recorder.begin_subsegment('mock-data')  
+    # subsegment.put_metadata('key', dict, 'namespace')
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -28,11 +28,11 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
-    subsegment = xray_recorder.begin_subsegment('mock-data')  
-    dict = {
-        "now":now.isoformat(),
-        "results-size": len(model['data'])
-    }
-    subsegment.put_metadata('key', dict, 'namespace')
+    # subsegment = xray_recorder.begin_subsegment('mock-data')  
+    # dict = {
+    #     "now":now.isoformat(),
+    #     "results-size": len(model['data'])
+    # }
+    # subsegment.put_metadata('key', dict, 'namespace')
     return model
     
