75a9e5a925f3b7418964e2d644787016abc563c7
Rewago3 <rewago3@gmail.com>
2023-04-10 23:05:12 +0000

    changes to schema prod

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Revathy Gadde', 'RevaGadde' ,'MOCK'),
+  ('Arjun Gadde', 'Akoth' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
